f30f4f16f76740fd2ac5340e637931d29be1b8eb
RithmaRandima <rithma.r.nandasiri.com>
2025-02-13 23:05:06 +0530

    Brand Section Completed

--- src/components/brand/Brand.jsx
+++ src/components/brand/Brand.jsx
@@ -1,8 +1,31 @@
 import React from "react";
 import "./Brand.css";
+import brand01 from "../../assets/brand-01.png";
+import brand02 from "../../assets/brand-02.png";
+import brand03 from "../../assets/brand-03.png";
+import brand04 from "../../assets/brand-04.png";
+import brand05 from "../../assets/brand-05.png";
 
 const Brand = () => {
-  return <div>Brand</div>;
+  return (
+    <div className="brand section__padding ">
+      <div>
+        <img src={brand01} alt="" />
+      </div>
+      <div>
+        <img src={brand02} alt="" />
+      </div>
+      <div>
+        <img src={brand03} alt="" />
+      </div>
+      <div>
+        <img src={brand04} alt="" />
+      </div>
+      <div>
+        <img src={brand05} alt="" />
+      </div>
+    </div>
+  );
 };
 
 export default Brand;
